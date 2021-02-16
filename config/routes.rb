5a13793c2133071ad4374370a4bdd219deae3f6e
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-02-16 08:50:14 +0000

    コントローラ導入前まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+
+  namespace :admin do
+    root to: 'homes#top'
+  end
+  scope module: :customer do
+    root to: 'homes#top'
+  end
+  
 end
