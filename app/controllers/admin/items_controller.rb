02a7aac1054155b0d27867450aaa2473c0323d7d
ochiaihitomi <sky.force.1013@gmail.com>
2021-02-17 08:37:38 +0000

    itemコントローラー作成中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,26 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
 
   def edit
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :title, :body, :genre, :non_tax_price, :sale_state)
   end
 end
