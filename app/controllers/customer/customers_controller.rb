95a16be4d100b8c79c44ef83d71ccc51f44b4822
EC2 Default User <ec2-user@ip-172-31-95-127.ec2.internal>
2021-02-17 15:13:10 +0000

    add[veiw]

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,8 +1,20 @@
 class Customer::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
+  end
+  
+  def update
+    @customer = customer.find(params[:id])
+    if @customer.update(user_params)
+     flash[:notice] = "You have updated user successfully."
+     redirect_to customer_path(@customer.id)
+    else
+     render("customer/edit")
+    end
   end
 
   def exit
