9e6c9512678ec3a9beae44fac4a09c5b201d3ded
ochiaihitomi <sky.force.1013@gmail.com>
2021-02-18 08:30:05 +0000

    item編集、マイグレーションファイル追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ItemsController < ApplicationController
-  
+# before_action :authenticate_user!を追加
+
   def index
     @items = Item.all
   end
@@ -14,11 +15,24 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
+    if @item.save
     redirect_to admin_items_path
+    else
+    render "new.html.erb"
+    end
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
+    else
+    render "edit.html.erb"
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
-  belongs_to :genre
+  #  belongs_to :genreを追加
   attachment :image
   has_many :orderd_items, dependent: :destroy
   has_many :cart_items, dependent: :destroy
+  
+  validates :title, presence: true
+  validates :non_tax_price, presence: true
+  
+  # validates :genre_id, presence: true　を追加
+  
 end
