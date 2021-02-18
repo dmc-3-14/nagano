1974b9850033f23a8ceba6268889b722c4446e22
EC2 Default User <ec2-user@ip-172-31-95-127.ec2.internal>
2021-02-18 08:37:06 +0000

    add[view]

--- app/controllers/customer/shippings_controller.rb
+++ app/controllers/customer/shippings_controller.rb
@@ -1,7 +1,31 @@
 class Customer::ShippingsController < ApplicationController
-  def edit
+
+
+  def create
+    shipping = Shipping.new(shipping_params)
+    shipping.save
+    redirect_to shippings_path
   end
 
   def index
+    @shippings = Shipping.all
+    @shipping = current_customer
   end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def shipping_params
+    params.permit(:post_code, :address, :name)
+  end
+
 end
