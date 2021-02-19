7195c6822cddbc24460984fa7736c8cdd415b04f
EC2 Default User <ec2-user@ip-172-31-68-245.ec2.internal>
2021-02-19 00:43:44 +0000

    test

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,4 +1,33 @@
 class Customer::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+  
   def index
+    @cart_items = current_customer.cart_items
+  end
+  
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.custamer_id = current_cusutomer.id
+    @cart_item.save
+    redirect_to cart_items_pathe
+  end
+ 
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    @custamer = Customer.find(current_cusutomer.id)
+    redirect_to cart_items_path
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+  　cart_item.destroy
+    redirect_to cart_items_path
+  end
+  
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:custamer_id, :item_id, :amount)
   end
 end
