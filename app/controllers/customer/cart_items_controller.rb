e64aaa67936e41dcc1063274ff914766469caaef
EC2 Default User <ec2-user@ip-172-31-68-245.ec2.internal>
2021-02-20 01:05:47 +0000

    cart_itemdestroy機能エラー

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -2,32 +2,36 @@ class Customer::CartItemsController < ApplicationController
   before_action :authenticate_customer!
   
   def index
-    @cart_items = current_customer.cart_items
+    @cart_items = CartItem.all
   end
   
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.custamer_id = current_cusutomer.id
+    @cart_item.customer_id = current_customer.id
     @cart_item.save
-    redirect_to cart_items_pathe
+    redirect_to cart_items_path
   end
  
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
-    @custamer = Customer.find(current_cusutomer.id)
     redirect_to cart_items_path
   end
 
   def destroy
     @cart_item = CartItem.find(params[:id])
-  　cart_item.destroy
+  　@cart_item.destroy
+    redirect_to cart_items_path
+  end
+  
+  def alldestroy
+    CartItem.destroy_all
     redirect_to cart_items_path
   end
   
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:custamer_id, :item_id, :amount)
+    params.require(:cart_item).permit(:customer_id, :item_id, :amount)
   end
 end
