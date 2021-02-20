a830f9c879446efe19763b817349aa2c2d25e8ba
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-02-20 10:06:25 +0000

    amountエラー修正

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,17 +1,17 @@
 class Customer::CartItemsController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items
   end
-  
+
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     @cart_item.save
     redirect_to cart_items_path
   end
- 
+
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
@@ -23,12 +23,12 @@ class Customer::CartItemsController < ApplicationController
     @cart_item.destroy
     redirect_to cart_items_path
   end
-  
+
   def alldestroy
     CartItem.destroy_all
     redirect_to cart_items_path
   end
-  
+
   private
 
   def cart_item_params
