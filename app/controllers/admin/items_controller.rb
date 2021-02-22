abb4c9d596efc5c835cf2895404435f7dd2f503b
EC2 Default User <ec2-user@ip-172-31-95-127.ec2.internal>
2021-02-22 05:16:21 +0000

    update

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,7 +2,7 @@ class Admin::ItemsController < ApplicationController
 before_action :authenticate_admin!
 
   def index
-    @items = Item.all
+    @items = Item.page(params[:page]).per(10)
     @item = Item.new
   end
 
@@ -17,7 +17,7 @@ before_action :authenticate_admin!
   def create
     @item = Item.new(item_params)
     if @item.save
-    redirect_to admin_items_path
+    redirect_to admin_item_path(@item.id)
     else
     render "new.html.erb"
     end
