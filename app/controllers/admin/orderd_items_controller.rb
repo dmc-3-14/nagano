e9c258507f50a35e335e83aac5a74ce29b5fc74a
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-02-22 02:41:51 +0000

    admin/order/show未完成

--- app/controllers/admin/orderd_items_controller.rb
+++ app/controllers/admin/orderd_items_controller.rb
@@ -1,13 +1,14 @@
 class Admin::OrderdItemsController < ApplicationController
+
   def update
     @orderd_item = OrderdItem.find(params[:id])
     @order = @orderd_item.order
     @orderd_item.update
 
     if @order_item.making_status == "製作中" #製作ステータスが「製作作中」なら入る
-			@order.update(order_status: 2)
+      @order.update(order_status: 2)
 	  end
-  end
+	end
 
   private
 
